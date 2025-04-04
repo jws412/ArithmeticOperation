0feb2a4d4fda29f2c61bc685137d5214edff9730
William Charron-Boyle <41563307+jws412@users.noreply.github.com>
2025-04-04 16:11:07 -0400

    revert changes to ArithmeticOperationsTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -17,7 +17,7 @@ public class ArithmeticOperationsTest {
 		assertEquals(expected, actual);
 
 	}
-	/*
+	
     @Test
     public void testSub()
     {
@@ -27,6 +27,6 @@ public class ArithmeticOperationsTest {
         Integer expected = 4;
         assertEquals(expected, actual);
 
-    }*/
+    }
 
 }
